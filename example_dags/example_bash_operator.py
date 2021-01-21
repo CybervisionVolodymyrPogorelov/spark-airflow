0d7a3ec20ecd0d638e3d02ec329367a4582a9cd2
Arkadii Kravchuk <akravchuk@cybervisiontech.com>
2021-01-21 20:54:25 +0200

    try sleep 5

--- example_dags/example_bash_operator.py
+++ example_dags/example_bash_operator.py
@@ -46,7 +46,7 @@ run_this_last = DummyOperator(
 # [START howto_operator_bash]
 run_this = BashOperator(
     task_id='run_after_loop',
-    bash_command='sleep 1',
+    bash_command='sleep 5',
     dag=dag,
 )
 # [END howto_operator_bash]
