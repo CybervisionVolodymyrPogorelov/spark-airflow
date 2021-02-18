9454a23b2ce64d164c3b9ce0afbc99b2520a895a
Arkadii Kravchuk <akravchuk@cybervisiontech.com>
2021-02-18 22:35:12 +0200

    K8S-2138 Addapt examples

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -51,7 +51,7 @@ with DAG(
         namespace='default',
         image="ubuntu:16.04",
         cmds=["bash", "-cx"],
-        arguments=["echo", "10"],
+        arguments=["echo", "sample"],
         labels={"foo": "bar"},
         name="airflow-test-pod",
         task_id="task",
